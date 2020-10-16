7dd579b8fab3c7fc47445bb5a905b10319aad8de
Martin Mazur <m.m@upplopp.se>
2020-10-16 17:15:41 +0200

    Dynamic generation of disco tiles!

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const MINCOLOR = parseInt("000000", 16);
-const MAXCOLOR = parseInt("FFFFFF", 16);
+(function (w, d) {
+  const MINCOLOR = parseInt("000000", 16);
+  const MAXCOLOR = parseInt("FFFFFF", 16);
+  const DANCEFLOOR = d.getElementById("dance-floor");
+  const AUDIO = d.getElementById('audio');
 
-function randomColor(){
-    let colorstring = "#" + (Math.floor(Math.random()*MAXCOLOR)+MINCOLOR).toString(16);
-    return colorstring;
-}
+  function randomColor(){
+      let colorstring = "#" + (Math.floor(Math.random()*MAXCOLOR)+MINCOLOR).toString(16);
+      return colorstring;
+  }
 
-function colorElements(HTMLCollection, colorfunction){
-    for(let i=0; i<HTMLCollection.length; i++){
-        HTMLCollection[i].style.backgroundColor = colorfunction()
-        ;;
+  function colorElements(HTMLCollection, colorfunction){
+      for(let i=0; i<HTMLCollection.length; i++){
+          HTMLCollection[i].style.backgroundColor = colorfunction()
+          ;;
+      }
+  }
+
+  function init() {
+    for(let i=0; i<50; i++){
+      DANCEFLOOR.appendChild(d.createElement("div"));
     }
-}
-const divs = document.getElementsByTagName('div');
-setInterval(() => colorElements(divs, randomColor), 500);
 
-const audio = document.getElementById('audio')
-audio.load();
+    setInterval(() => colorElements(DANCEFLOOR.children, randomColor), 500);
+
+    AUDIO.load();
+  }
+
+  init();
+}(window, document));
